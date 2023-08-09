359ea6263dfd38f5532f614de2855357c807a079
Sharjeel Mazhar <sharjeelmazhar@protonmail.com>
2023-08-09 13:26:08 +0400

    update Home screen UI

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -6,6 +6,60 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey.shade900,
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          // crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Expanded(
+              flex: 8,
+              child: Center(
+                child: Text(
+                  'Questions are shown here',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+            Expanded(
+              child: TextButton(
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.green, // Background Color
+                ),
+                // style: const ButtonStyle(textStyle: TextStyle(color: Colors.red)),
+                // onPressed: () => print('Yes pressed'),
+                // onLongPress: () => print('Yes long pressed'),
+                onPressed: null,
+                child: const Center(
+                  child: Text(
+                    'Yes',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Expanded(
+              child: TextButton(
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.red, // Background Color
+                ),
+                onPressed: null,
+                child: const Center(
+                  child: Text(
+                    'No',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 50,
+            )
+          ],
+        ),
+      ),
     );
   }
 }
