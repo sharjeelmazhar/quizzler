4c27c5049cfb06b901342ddc0340e4e112e108fc
Sharjeel Mazhar <sharjeelmazhar@protonmail.com>
2023-08-11 18:18:38 +0400

    Hide debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Quizzler',
       theme: lightTheme,
       darkTheme: darkTheme,
